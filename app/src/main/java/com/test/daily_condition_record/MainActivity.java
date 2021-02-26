b8c63cf82207d9cc689ff3f978f2871b158f9242
hyeals <phm0496@ajou.ac.kr>
2021-02-26 14:43:07 +0900

    Add weather textView

--- app/src/main/java/com/test/daily_condition_record/MainActivity.java
+++ app/src/main/java/com/test/daily_condition_record/MainActivity.java
@@ -1,7 +1,17 @@
 package com.test.daily_condition_record;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+
+        TextView toolbar_Text = findViewById(R.id.toolbar_Text);
+        toolbar_Text.setText("년도 받아오기 ex: 2021");
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toolbar_plus_button: // 피드 추가 기능 버튼
+                Intent intent = new Intent(this, PostActivity.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/test/daily_condition_record/PostActivity.java
+++ app/src/main/java/com/test/daily_condition_record/PostActivity.java
@@ -1,8 +1,13 @@
 package com.test.daily_condition_record;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home: // back button ID
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
