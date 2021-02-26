e9d9e354a4e52f99bdb0bca4d8bf6c59954167dc
hyeals <phm0496@ajou.ac.kr>
2021-02-26 10:15:42 +0900

    Add weather textView

--- app/src/main/java/com/test/daily_condition_record/PostActivity.java
+++ app/src/main/java/com/test/daily_condition_record/PostActivity.java
@@ -1,15 +1,14 @@
 package com.test.daily_condition_record;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_post);
     }
-
 }
